55347c8e019660f2da49868c95fca22463e4bc26
Bacotell36 <ryan@fixdapp.com>
2023-04-21 17:12:19 -0400

    switch to meltano 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="target-postgres",
-    version="meltano.2.0.0",
+    version="meltano.1.1.0",
     description="Meltano maintained fork of target-postgres - Singer.io target for Postgres",
     author="Meltano and Statsbot",
     url="https://statsbot.co",
